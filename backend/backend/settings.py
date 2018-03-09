dc425d1142ec4389f4e066d9fda98e4ea99d778d
junaid <junaid1460@gmail.com>
2018-03-09 17:41:43 +0530

    update cred

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -75,8 +75,11 @@ WSGI_APPLICATION = 'backend.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE'    : 'django_mongodb_engine',
+        'NAME'      : 'my_database',
+        'HOST'      : '10.10.10.2'
+        'USER'      : 'junaid'
+        'password'  : 'root'
     }
 }
 
