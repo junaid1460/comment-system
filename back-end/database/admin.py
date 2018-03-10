b67efe442014859f392a8a074d2f655144af1614
junaid <junaid1460@gmail.com>
2018-03-10 21:13:45 +0530

    register models with admin site

--- back-end/database/admin.py
+++ back-end/database/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
 
 # Register your models here.
+
+from database.models import Comment, Post, Reply
+
+for model in [Comment, Post, Reply]:
+    admin.site.register(model)
