bf6498932042c2dad813dbb775d4f0e56dae1035
junaid <junaid1460@gmail.com>
2018-03-11 22:12:46 +0530

    menu component

---
+++ front-end/src/menu.js
@@ -0,0 +1,94 @@
+import React, { Component } from 'react';
+import logo from './logo.svg';
+import './App.css';
+import ListSubheader from 'material-ui/List/ListSubheader';
+import AppBar from 'material-ui/AppBar' 
+import Toolbar from 'material-ui/Toolbar';
+import Input, { InputLabel } from 'material-ui/Input';
+import TextField from 'material-ui/TextField';
+import { FormControl } from 'material-ui/Form';
+// import IconButton from 'material-ui/IconButton';
+import MenuIcon from 'material-ui-icons/Menu';
+import AccountCircle from 'material-ui-icons/AccountCircle';
+import Switch from 'material-ui/Switch';
+import { FormControlLabel, FormGroup } from 'material-ui/Form';
+import Menu, { MenuItem } from 'material-ui/Menu';
+// import Typography from 'material-ui/Typography';
+import Paper from 'material-ui/Paper';
+import Button from 'material-ui/Button';
+import Fade from 'material-ui/transitions/Fade';
+
+import Card, { CardHeader, CardMedia, CardContent, CardActions } from 'material-ui/Card';
+import Collapse from 'material-ui/transitions/Collapse';
+import Avatar from 'material-ui/Avatar';
+import IconButton from 'material-ui/IconButton';
+import Typography from 'material-ui/Typography';
+import red from 'material-ui/colors/red';
+import FavoriteIcon from 'material-ui-icons/Favorite';
+import ShareIcon from 'material-ui-icons/Share';
+import ExpandMoreIcon from 'material-ui-icons/ExpandMore';
+import MoreVertIcon from 'material-ui-icons/MoreVert';
+import { BrowserRouter as Router, Switch as Sswitch, Route, Link } from 'react-router-dom';
+
+import { withStyles } from 'material-ui/styles';
+// import Button from 'material-ui/Button';
+// import Avatar from 'material-ui/Avatar';
+// import List, { ListItem, ListItemAvatar, ListItemText } from 'material-ui/List';
+import Dialog, { DialogTitle } from 'material-ui/Dialog';
+import PersonIcon from 'material-ui-icons/Person';
+import AddIcon from 'material-ui-icons/Add';
+// import Typography from 'material-ui/Typography';
+import blue from 'material-ui/colors/blue';
+
+import List, {
+  ListItem,
+  ListItemAvatar,
+  ListItemIcon,
+  ListItemSecondaryAction,
+  ListItemText,
+} from 'material-ui/List';
+
+
+class MyMenu extends Component {
+    state = {
+      anchorEl: null,
+    };
+    handleClick = event => {
+      this.setState({ anchorEl: event.currentTarget });
+    };
+    handleClose = () => {
+      this.setState({ anchorEl: null });
+    };
+  
+    render() {
+      const { anchorEl } = this.state;
+        if (this.props.test) {
+  
+            return  <div>
+                  <IconButton aria-owns={anchorEl ? 'fade-menu' : null}
+            aria-haspopup="true"
+                    onClick={this.handleClick}
+                  >
+                    <MoreVertIcon/>
+                  </IconButton>
+                  <Menu
+                    id="fade-menu"
+                    anchorEl={anchorEl}
+                    open={Boolean(anchorEl)}
+                    onClose={this.handleClose}
+                    transition={Fade}
+                    onClick = {this.handleClose}
+                  >
+                   { this.props.children }
+  
+                  </Menu>
+          </div>
+        }
+        else {
+            return false;
+        }
+    }
+  };
+
+
+  export default MyMenu
