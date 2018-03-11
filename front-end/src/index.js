60d125b0f458cefcf6417b317ac51162e8e0e96b
junaid <junaid1460@gmail.com>
2018-03-11 12:38:49 +0530

    update compoenent name

--- front-end/src/index.js
+++ front-end/src/index.js
@@ -1,8 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
+import RR from './App';
 import registerServiceWorker from './registerServiceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(<RR />, document.getElementById('root'));
 registerServiceWorker();
