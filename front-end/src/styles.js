b83594193a74e74f494150c2761a69c33d216e15
junaid <junaid1460@gmail.com>
2018-03-11 22:13:50 +0530

    move styles to another file

---
+++ front-end/src/styles.js
@@ -0,0 +1,38 @@
+const styles = {
+    Toolbar: {
+    },
+   paper: {
+      margin : '10vw auto',
+      minHeight : '70vh',
+      // padding : '10px',
+      zIndex: '10',
+      position : 'absolute',
+      minWidth : '90vw'
+    },
+    container :{
+      overflowY: 'auto',
+      height: '100vh',
+      display : 'flex',
+      position: 'absolute',
+      width: '100vw',
+      justifyContent: 'center'
+    },
+    list : {
+      margin: '10px'
+    },
+    card : {
+      width : '100%'
+    },
+    actions : {
+      display: 'flex',
+      alignItems: 'right',
+      justifyContent: 'right'
+    },
+    btn : {
+      color: 'white',
+      textDecoration : 'none'
+    },
+    rep:{minWidth: '10px',fontSize : '7pt',padding :'0px', color: 'gray', minHeight : '20px', marginRight: '8px'}
+  };
+
+  export default styles;
