540833a27cd594b688bf8fa0b9545eabf94f1dc4
junaid <junaid1460@gmail.com>
2018-03-11 11:05:51 +0530

    register login handler url

--- back-end/backend/urls.py
+++ back-end/backend/urls.py
@@ -15,11 +15,12 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
-from api.views import router, loggedIn
+from api.views import router, loggedIn, login
 from rest_framework import routers
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path(r'api/', include('rest_framework.urls', namespace='rest_framework')),
+    path('login/', login),
+    path('api/', include('rest_framework.urls', namespace='rest_framework')),
     path('api/auth', loggedIn)
 ]
 r = routers.DefaultRouter()
