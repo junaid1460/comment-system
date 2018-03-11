2a06099767571b80c275ca0ca65d370c68bd8bb7
junaid <junaid1460@gmail.com>
2018-03-11 06:54:04 +0530

    add auth check route

--- back-end/backend/urls.py
+++ back-end/backend/urls.py
@@ -15,11 +15,12 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
-from api.views import router
+from api.views import router, loggedIn
 from rest_framework import routers
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path(r'api/', include('rest_framework.urls', namespace='rest_framework'))
+    path(r'api/', include('rest_framework.urls', namespace='rest_framework')),
+    path('api/auth', loggedIn)
 ]
 r = routers.DefaultRouter()
 for route in router:
