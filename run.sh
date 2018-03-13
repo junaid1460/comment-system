34dc192a010122072226e96a816f6a64a241199e
junaid <junaid1460@gmail.com>
2018-03-13 19:41:26 +0530

    auto runscript for Linux

--- run.sh
+++ run.sh
@@ -1,1 +1,15 @@
 #!/bin/bash
+
+bash ./build.sh
+freemem=`cat /proc/meminfo |grep MemFree|xargs  echo|cut -d' ' -f2`
+freemem=$(($freemem / 1000 ))
+if [ "$(( $freemem > 1500 ))" == "1" ]
+then
+    echo "Having enough free memory : running all services at once"
+    docker-compose up build-and-run
+else
+    echo "Not enough menory: building static files first and then running server"    
+    docker-compose up build-static
+    docker-compose up server    
+fi
+
